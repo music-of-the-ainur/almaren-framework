563b92a0e74bfc4ed0271b62448742a13c4c4d8d
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-09-26 22:52:59 -0300

    add core

---
+++ src/main/scala/com/github/music/of/the/ainur/almaren/core/Core.scala
@@ -0,0 +1,74 @@
+package com.github.music.of.the.ainur.almaren.core
+
+import org.apache.spark.sql.DataFrame
+import com.github.music.of.the.ainur.almaren.util.Constants
+import org.apache.spark.sql.types.{StructType, StructField, StringType}
+
+abstract class Core extends State {
+  override def state(df: DataFrame): DataFrame = core(df)
+  def core(df: DataFrame): DataFrame
+}
+
+class SQLState(sql: String) extends Core {
+  override def core(df: DataFrame): DataFrame = sql(df)
+  def sql(df: DataFrame): DataFrame = {
+    logger.info(s"sql:{$sql}")
+    df.createOrReplaceTempView(Constants.TempTableName)
+    df.sqlContext.sql(sql)
+  }
+}
+
+class Coalesce(size:Int) extends Core {
+  override def core(df: DataFrame): DataFrame = coalesce(df)
+  def coalesce(df: DataFrame): DataFrame = {
+    logger.info(s"{$size}")
+    df.coalesce(size)
+  }
+}
+
+class Repartition(size:Int) extends Core {
+  override def core(df: DataFrame): DataFrame = repartition(df)
+  def repartition(df: DataFrame): DataFrame = {
+    logger.info(s"{$size}")
+    df.repartition(size)
+  }
+}
+
+class Pipe(command:String) extends Core {
+  override def core(df: DataFrame): DataFrame = pipe(df)
+  def pipe(df: DataFrame): DataFrame = {
+    import df.sparkSession.implicits._
+    logger.info(s"{$command}")
+    df.rdd.pipe(command).toDF()
+  }
+}
+
+class Alias(alias:String) extends Core {
+  override def core(df: DataFrame): DataFrame = alias(df)
+  def alias(df: DataFrame): DataFrame = {
+    logger.info(s"{$alias}")
+    df.createOrReplaceTempView(alias)
+    df
+  }
+}
+
+class Cache(opType:Boolean,tableName:Option[String]) extends Core {
+  override def core(df: DataFrame): DataFrame = cache(df)
+  def cache(df: DataFrame): DataFrame = {
+    logger.info(s"opType:{$opType}, tableName{$tableName}")
+    tableName match {
+      case Some(t) => cacheTable(df,t)
+      case None => cacheDf(df)
+    }
+    df
+  }
+  private def cacheDf(df:DataFrame): Unit = opType match {
+    case true => df.cache()
+    case false => df.unpersist()
+  }
+  private def cacheTable(df:DataFrame,tableName: String): Unit =
+    opType match {
+      case true => df.sqlContext.cacheTable(tableName)
+      case false => df.sqlContext.uncacheTable(tableName)
+    }
+}
---
+++ src/main/scala/com/github/music/of/the/ainur/almaren/core/Deserializer.scala
@@ -0,0 +1,39 @@
+package com.github.music.of.the.ainur.almaren.core
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types.{DataType, StructType}
+import scala.language.implicitConversions
+
+abstract class Deserializer() extends State {
+  override def state(df: DataFrame): DataFrame = deserializer(df)
+  def deserializer(df: DataFrame): DataFrame
+  implicit def string2Schema(schema: String): DataType =
+    StructType.fromDDL(schema)
+}
+
+class AvroDeserializer(columnName: String,schema: String) extends Deserializer {
+  import org.apache.spark.sql.functions._
+  import org.apache.spark.sql.avro._
+  override def deserializer(df: DataFrame): DataFrame = {
+    logger.info(s"columnName:{$columnName}, schema:{$schema}")
+    df.withColumn(columnName,from_avro(col(columnName),schema))
+      .select("*",columnName.concat(".*")).drop(columnName)
+  }
+}
+
+class JsonDeserializer(columnName: String,schema: String) extends Deserializer {
+  import org.apache.spark.sql.functions._
+  override def deserializer(df: DataFrame): DataFrame = {
+    logger.info(s"columnName:{$columnName}, schema:{$schema}")
+    df.withColumn(columnName,from_json(col(columnName),schema))
+      .select("*",columnName.concat(".*")).drop(columnName)
+  }
+}
+
+class XMLDeserializer(columnName: String) extends Deserializer {
+  import com.databricks.spark.xml.XmlReader
+  override def deserializer(df: DataFrame): DataFrame = {
+    logger.info(s"columnName:{$columnName}")
+    new XmlReader().xmlRdd(df.sparkSession,df.select(columnName).rdd.map(r => r(0).asInstanceOf[String])).toDF
+  }
+}

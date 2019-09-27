63bca0901472fe6faec2b136b7f20e4064d63956
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-09-26 22:51:33 -0300

    add Constants

---
+++ src/main/scala/com/github/music/of/the/ainur/almaren/util/Constants.scala
@@ -0,0 +1,7 @@
+package com.github.music.of.the.ainur.almaren.util
+
+object Constants {
+  val TempTableName = "__TABLE__"
+  val TempStreamTableName = "__STREAMING__"
+  val FrameworkName = "Almaren Framework"
+}

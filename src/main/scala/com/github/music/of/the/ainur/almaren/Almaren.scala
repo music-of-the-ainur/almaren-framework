e202dbe93a5c4151feca95b2ffb9abc792cc0e9a
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-09-26 22:51:41 -0300

    add sparkSession and Config

--- src/main/scala/com/github/music/of/the/ainur/almaren/Almaren.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/Almaren.scala
@@ -1,8 +1,13 @@
 package com.github.music.of.the.ainur.almaren
 
 import com.typesafe.scalalogging.LazyLogging
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 object Almaren extends LazyLogging {
+
+  val sparkConf = new SparkConf
+  lazy val spark = SparkSession.builder().config(sparkConf)
 
 }
 

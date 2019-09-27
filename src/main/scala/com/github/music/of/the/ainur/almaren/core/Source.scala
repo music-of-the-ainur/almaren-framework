cd36f465a199768724b75420f27164097f039ad9
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-09-26 22:59:57 -0300

    add source

---
+++ src/main/scala/com/github/music/of/the/ainur/almaren/core/Source.scala
@@ -0,0 +1,16 @@
+package com.github.music.of.the.ainur.almaren.core
+
+import org.apache.spark.sql.DataFrame
+
+abstract class Source() extends State {
+  override def state(df: DataFrame): DataFrame = source(df)
+  def source(df: DataFrame): DataFrame
+}
+
+class SourceSql(sql: String) extends Source {
+  override def source(df: DataFrame): DataFrame = {
+    logger.info(s"sql:{$sql}")
+    val sqlDf = df.sparkSession.sql(sql)
+    sqlDf
+  }
+}

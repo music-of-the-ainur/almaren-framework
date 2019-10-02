4c33b2caae3cb808166fd62f6955aad36fef82f7
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-10-02 14:59:09 -0300

    add builder

--- src/main/scala/com/github/music/of/the/ainur/almaren/Builder.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/Builder.scala
@@ -1,8 +1,23 @@
-package com.github.music.of.the.ainur.almaren.core
+package com.github.music.of.the.ainur.almaren
 
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.spark.sql.DataFrame
+import com.github.music.of.the.ainur.almaren.core._
 
 private[almaren] trait Builder extends LazyLogging with Serializable {
+
+  var tree:Option[Tree] = None
+
+  def sourceSql(sql: String):State = {
+    new SourceSql(sql)
+  }
+
+  private[almaren] def build(state: State): Option[Tree] = {
+    tree match {
+      case Some(t) => t // Complex build logic will be here
+      case None => tree = Some(Tree(state)) 
+    }
+    tree
+  }
 
 }

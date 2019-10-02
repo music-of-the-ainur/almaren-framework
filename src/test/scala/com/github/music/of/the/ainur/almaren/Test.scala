8f22a49813ddb44d6f30b7f7c795e970aa8a725c
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-10-02 19:30:44 -0300

    add test

--- src/test/scala/com/github/music/of/the/ainur/almaren/Test.scala
+++ src/test/scala/com/github/music/of/the/ainur/almaren/Test.scala
@@ -1,16 +1,28 @@
 package com.github.music.of.the.ainur.almaren
 
 import com.github.music.of.the.ainur.almaren.core._
+import org.scalatest._
 
-class Test {
+class Test extends FunSuite with BeforeAndAfter {
   val almaren = Almaren("App Test")
+  val spark = almaren.spark.master("local[*]").getOrCreate()
+
+  val json_str = scala.io.Source.fromURL(getClass.getResource("/sample_data/movies.json")).mkString
+  import spark.implicits._
+
+  val res = spark.read.json(Seq(json_str).toDS)
+  res.show
+  res.printSchema()
+  res.createTempView("movies")
 
   val tree = Tree(
-    new SourceSql("select * from foo"),
-    List(Tree(new JsonDeserializer("foo","bar"),
+    new SourceSql("select * from movies"),
+    List(Tree(new Cache(true,None),
       List(
-        Tree(new SQLState("select 1")),
-        Tree(new SQLState("select 2"))
+        Tree(new SQLState("select year from __TABLE__")),
+        Tree(new SQLState("select title from __TABLE__")),
+        Tree(new SQLState("select genres from __TABLE__"), 
+          List(Tree(new SQLState("select genres,count(*) from (select explode_outer(genres) as genres from __TABLE__) G where genres is not null group by genres order by count(*) desc"))))
       )
     ))
   )

a06765c239d621da9f56f6d998bbf1c9c8805f5a
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-10-02 14:15:27 -0300

    add private

--- src/main/scala/com/github/music/of/the/ainur/almaren/Executor.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/Executor.scala
@@ -2,7 +2,7 @@ package com.github.music.of.the.ainur.almaren
 
 import org.apache.spark.sql.DataFrame
 
-trait Executor {
+private[almaren] trait Executor {
   def catalyst(tree: Tree,df: DataFrame = Almaren.spark.getOrCreate().emptyDataFrame): DataFrame =
     tree match {
       case Tree(s, list) if list.nonEmpty => list.foldLeft(s.executor(df))((d, t) => catalyst(t,d))

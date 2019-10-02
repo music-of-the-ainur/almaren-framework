f8a003ac1ded681195e95cf2f32fbc141ea7d43a
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-10-02 14:12:30 -0300

    fixed bug

--- src/main/scala/com/github/music/of/the/ainur/almaren/Executor.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/Executor.scala
@@ -5,8 +5,8 @@ import org.apache.spark.sql.DataFrame
 trait Executor {
   def catalyst(tree: Tree,df: DataFrame = Almaren.spark.getOrCreate().emptyDataFrame): DataFrame = {
     tree match {
-      case Tree(s, list) if list.nonEmpty => list.foldLeft(tree.state.executor(df))((d, t) => catalyst(t,d))
-      case Tree(s, list) => tree.state.executor(df)
+      case Tree(s, list) if list.nonEmpty => list.foldLeft(s.executor(df))((d, t) => catalyst(t,d))
+      case Tree(s, list) => s.executor(df)
     }
   }
 }

8610d2358eaacd48a10930edfcd78e33fd94405c
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-10-02 14:08:02 -0300

    add executor to almaren

--- src/main/scala/com/github/music/of/the/ainur/almaren/Almaren.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/Almaren.scala
@@ -4,15 +4,11 @@ import com.typesafe.scalalogging.LazyLogging
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
-object Almaren extends LazyLogging {
+object Almaren extends LazyLogging with Executor {
 
   def apply(appName: String) = { sparkConf.setAppName(appName);Almaren}
 
   val sparkConf = new SparkConf
   lazy val spark = SparkSession.builder().config(sparkConf)
 
-}
-
-class Almaren() {
- 
 }
--- src/main/scala/com/github/music/of/the/ainur/almaren/Tree.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/Tree.scala
@@ -1,5 +1,3 @@
 package com.github.music.of.the.ainur.almaren
 
-import org.apache.spark.sql.DataFrame
-
 private[almaren] case class Tree(state: State, c: List[Tree] = List.empty)

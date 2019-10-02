dd4c2f78e97809b4be9a24af48d7f6dc21f9cc88
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-10-02 14:35:48 -0300

    clean up

--- src/test/scala/com/github/music/of/the/ainur/almaren/Test.scala
+++ src/test/scala/com/github/music/of/the/ainur/almaren/Test.scala
@@ -5,27 +5,17 @@ import com.github.music.of.the.ainur.almaren.core._
 class Test {
   val almaren = Almaren("App Test")
 
-
-/*
-  val node = Tree(
-    new SourceSql("select * from foo"),List(
-    Node(new JsonDeserializer("foo","bar")
-      Node(new SQLState("select 1")),Node(new SQLState("select 2"))
-    )))
-
- */
   val tree = Tree(
     new SourceSql("select * from foo"),
     List(Tree(new JsonDeserializer("foo","bar"),
       List(
         Tree(new SQLState("select 1")),
-        Tree(new SQLState("select 2")),
-      ))
+        Tree(new SQLState("select 2"))
+      )
     ))
+  )
 
   println {
-    almaren.catalyst(tree)
+    almaren.catalyst(tree).show(false)
   }
-
-
 }

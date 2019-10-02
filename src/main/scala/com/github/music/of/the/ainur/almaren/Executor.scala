25cf8191de0595e1eb3322c31a334ea9b34fcd88
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-10-02 14:13:22 -0300

    clean up

--- src/main/scala/com/github/music/of/the/ainur/almaren/Executor.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/Executor.scala
@@ -3,10 +3,9 @@ package com.github.music.of.the.ainur.almaren
 import org.apache.spark.sql.DataFrame
 
 trait Executor {
-  def catalyst(tree: Tree,df: DataFrame = Almaren.spark.getOrCreate().emptyDataFrame): DataFrame = {
+  def catalyst(tree: Tree,df: DataFrame = Almaren.spark.getOrCreate().emptyDataFrame): DataFrame =
     tree match {
       case Tree(s, list) if list.nonEmpty => list.foldLeft(s.executor(df))((d, t) => catalyst(t,d))
       case Tree(s, list) => s.executor(df)
-    }
-  }
+    }  
 }

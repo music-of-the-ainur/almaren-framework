2916d2804dab8e320a7e7652ca29960ef5b6427e
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-10-07 22:01:43 -0300

    clean up

--- src/main/scala/com/github/music/of/the/ainur/almaren/component/Executor.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/component/Executor.scala
@@ -6,12 +6,13 @@ import com.github.music.of.the.ainur.almaren.Almaren
 
 private[ainur] trait Executor {
   // execute's PreOrder BT
-  def catalyst(tree: Tree,df: DataFrame = Almaren.spark.getOrCreate().emptyDataFrame): DataFrame = 
+  def catalyst(tree: Tree,df: DataFrame = Almaren.spark.getOrCreate().emptyDataFrame, count:Int = 0): DataFrame = {
+    println(s"[+]$count")
     tree match {
-      case Tree(s, list) if list.nonEmpty => parentExec(list,s.executor(df))
+      case Tree(s, list) if list.nonEmpty => parentExec(list,s.executor(df),count + 1)
       case Tree(s, list) => s.executor(df)
     }
-
-  private def parentExec(tree: List[Tree],df: DataFrame): DataFrame = 
-    tree.foldLeft(df)((d,t) => catalyst(t,df))  
+  }
+  private def parentExec(tree: List[Tree],df: DataFrame, count:Int): DataFrame = 
+    tree.foldLeft(df)((d,t) => catalyst(t,df,count))  
 }

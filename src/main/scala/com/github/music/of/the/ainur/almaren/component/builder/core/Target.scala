ce190f40b43d021d1e3b1c8f0e6b4526fda1a818
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-10-10 00:50:34 -0300

    add targetJdbc

--- src/main/scala/com/github/music/of/the/ainur/almaren/component/builder/core/Target.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/component/builder/core/Target.scala
@@ -7,4 +7,7 @@ import com.github.music.of.the.ainur.almaren.component.state.core._
 private[almaren] trait Target extends Core {
   def targetSql(sql: String): Option[Container] = 
     new TargetSql(sql: String)
+
+  def targetJdbc(url: String, driver: String, query: String, params:Map[String,String] = Map[String,String]()): Option[Container] = 
+    new TargetJdbc(url,driver,query,params)
 }

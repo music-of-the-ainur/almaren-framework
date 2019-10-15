571bd740e07a33a0c18e2207293532538d222eb5
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2019-10-15 11:42:35 -0300

    add deserializer

--- src/main/scala/com/github/music/of/the/ainur/almaren/Exception.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/Exception.scala
@@ -9,3 +9,9 @@ case class NullCatalyst()
 
 case class NullFork() 
     extends Exception(s"Can't execute fork with an empty Container") with AlmarenException
+
+case class InvalidDecoder(decoder: String) 
+    extends Exception(s"Invalid decoder:{$decoder}") with AlmarenException
+
+case class SchemaRequired(decoder: String) 
+    extends Exception(s"Schema is mandatory for the decoder:{$decoder}") with AlmarenException
---
+++ src/main/scala/com/github/music/of/the/ainur/almaren/builder/core/Deserializer.scala
@@ -0,0 +1,26 @@
+package com.github.music.of.the.ainur.almaren.builder.core
+
+import com.github.music.of.the.ainur.almaren.Container
+import com.github.music.of.the.ainur.almaren.builder.Core
+import com.github.music.of.the.ainur.almaren.state.core._
+import com.github.music.of.the.ainur.almaren.{State,InvalidDecoder,SchemaRequired}
+
+private[almaren] trait Deserializer extends Core {
+  def deserializer(decoder:String,columnName:String,schemaInfo:Option[String] = None): State = {
+
+    def json(): State =
+      new JsonDeserializer(columnName,schemaInfo.getOrElse(throw SchemaRequired(decoder)))
+    def xml(): State =
+      new XMLDeserializer(columnName)
+    def avro(): State =
+      new AvroDeserializer(columnName,schemaInfo.getOrElse(throw SchemaRequired(decoder)))
+
+
+    decoder.toUpperCase match {
+      case "JSON" => json
+      case "XML" => xml
+      case "AVRO" => avro
+      case d => throw InvalidDecoder(d)
+    }
+  }
+}

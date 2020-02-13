e67b602a37b66fbf0a913dba1f73335e001857bb
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2020-02-12 19:39:00 -0500

    fixed json warning

--- src/main/scala/com/github/music/of/the/ainur/almaren/state/core/Deserializer.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/state/core/Deserializer.scala
@@ -7,6 +7,7 @@ import org.apache.spark.sql.types.{DataType, StructType}
 import scala.language.implicitConversions
 import com.github.music.of.the.ainur.almaren.Almaren
 import com.github.music.of.the.ainur.almaren.util.Constants
+import org.apache.spark.sql.Dataset
 
 abstract class Deserializer() extends State {
   override def executor(df: DataFrame): DataFrame = deserializer(df)
@@ -33,12 +34,12 @@ case class JsonDeserializer(columnName: String,schema: Option[String]) extends D
     logger.info(s"columnName:{$columnName}, schema:{$schema}")
     df.withColumn(columnName,
       from_json(col(columnName),
-        schema.getOrElse(getSchemaDDL(df.selectExpr(columnName)))))
+        schema.getOrElse(getSchemaDDL(df.selectExpr(columnName).as[(String)]))))
       .select("*",columnName.concat(".*"))
       .drop(columnName)
   }
-  private def getSchemaDDL(df: DataFrame): String =
-    Almaren.spark.getOrCreate().read.json(df.sample(Constants.sampleDeserializer).rdd.map(_.getString(0))).schema.toDDL
+  private def getSchemaDDL(df: Dataset[String]): String =
+    Almaren.spark.getOrCreate().read.json(df.sample(Constants.sampleDeserializer)).schema.toDDL
 }
 
 case class XMLDeserializer(columnName: String) extends Deserializer {

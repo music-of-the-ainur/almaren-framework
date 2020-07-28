c9962cbb2e6dd85724a2a286a7a3158f79c9e239
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2020-07-28 12:33:30 +0200

    fixed bug on sourceFile

--- src/main/scala/com/github/music/of/the/ainur/almaren/builder/core/Source.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/builder/core/Source.scala
@@ -12,6 +12,6 @@ private[almaren] trait Source extends Core {
   def sourceJdbc(url: String, driver: String, query: String, user: Option[String] = None, password: Option[String] = None, params: Map[String, String] = Map()): Option[Tree] =
     SourceJdbc(url, driver, query, user, password, params)
 
-  def sourceFile(format: String, path: String, params: Map[String, String] = Map()) =
+  def sourceFile(format: String, path: String, params: Map[String, String] = Map()): Option[Tree] =
     SourceFile(format,path,params)
 }

26c5500936ee3c3a0bc65e09964edae5b4dc52f0
badrinathpatchikolla <57659308+badrinathpatchikolla@users.noreply.github.com>
2022-03-21 20:06:14 +0530

    Fixed targetFile

--- src/main/scala/com/github/music/of/the/ainur/almaren/state/core/Target.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/state/core/Target.scala
@@ -75,10 +75,10 @@ case class TargetFile(format: String,
       write.bucketBy(bucketBy._1, bucketBy._2.head, bucketBy._2.tail: _*)
       if (sortBy.nonEmpty)
         write.sortBy(sortBy.head, sortBy.tail: _*)
-          .saveAsTable(tableName.getOrElse(throw new Exception("tableName not provided")))
-      else
-        write.save
+      write.saveAsTable(tableName.getOrElse(throw new Exception("tableName not provided")))
     }
+    else
+      write.save
     df
   }
 }
